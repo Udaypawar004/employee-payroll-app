74d425aaa37f05600086a115235fb728ac19e8ef
Uday Deshmukh <udaypawar004@gmail.com>
2025-02-27 11:14:55 +0530

    [Uday] Add. Section3 uc4 Database addition and checking

--- src/main/java/com/payroll/app/EmployeePayrollAppApplication.java
+++ src/main/java/com/payroll/app/EmployeePayrollAppApplication.java
@@ -11,7 +11,8 @@ public class EmployeePayrollAppApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(EmployeePayrollAppApplication.class, args);
-		log.info("Employee Payroll app started...", context.getEnvironment().getProperty("enviroment"));
+		log.info("Employee Payroll app started in {} Environment", context.getEnvironment().getProperty("environment"));
+		log.info("Employee Payroll DB User is {} ", context.getEnvironment().getProperty("spring.datasource.username"));
 	}
 
 }
